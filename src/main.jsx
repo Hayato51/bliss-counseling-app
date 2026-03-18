3851191ca48bc97d1d063676211b7ffafc2a8568
Hayato51 <hayato0501@gmail.com>
2026-03-18 09:56:47 +0900

    Add React Router with /survey/:id route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import SurveyPage from './pages/SurveyPage'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <BrowserRouter>
+              <Routes>
+                      <Route path="/survey/:id" element={<SurveyPage />} />
+                      <Route path="/*" element={<App />} />
+              </Routes>Routes>
+        </BrowserRouter>BrowserRouter>
+    </React.StrictMode>React.StrictMode>
+  )</React.StrictMode>
